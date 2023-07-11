f8fcb51f88e320ac9b77e1dbb8b3427e5be77c18
sudhersun <123535178+sudhersun@users.noreply.github.com>
2023-07-11 09:45:22 +0530

    First commit

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+// scroll reveal
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal(' .heading, .website-header, .install-content, .theme-heading, .launch-website-content', { origin: 'top' });
+ScrollReveal().reveal('.webpage-img, .install-option, .theme-option-col, .launch-button-col', { origin: 'bottom' });
